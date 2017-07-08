8673b1b97be5e03499e19579feb6ab432cbc6d6b
Ali Yazdi <ma.yazdi75@gmail.com>
2017-07-09 01:45:37 +0430

    added memory

---
+++ memory.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    01:30:20 07/09/2017 
+-- Design Name: 
+-- Module Name:    memory - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity memory is
+end memory;
+
+architecture Behavioral of memory is
+
+begin
+
+
+end Behavioral;
+
