15851142de741dcddfc32c7b006f3752794c80f9
Ali Yazdi <ma.yazdi75@gmail.com>
2017-07-09 02:00:35 +0430

    update

--- memory.vhd
+++ memory.vhd
@@ -19,6 +19,7 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -30,12 +31,64 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity memory is
-end memory;
+	generic (blocksize : integer := 1024);
 
-architecture Behavioral of memory is
+	port (clk, readmem, writemem : in std_logic;
+		addressbus: in std_logic_vector (9 downto 0);
+		databus : inout std_logic_vector (15 downto 0);
+		memdataready : out std_logic);
+end entity memory;
 
+architecture behavioral of memory is
+	type mem is array (0 to blocksize - 1) of std_logic_vector (15 downto 0);
 begin
+	process (clk)
+		variable buffermem : mem := (others => (others => '0'));
+		variable ad : integer;
+		variable init : boolean := true;
+	begin
+		if init = true then
+			-- some initiation
+			-- cwp
+			buffermem(0) := "0010000100000000";
 
+			-- mil r0, 01011101
+			buffermem(1) := "0011000100000000";
 
-end Behavioral;
+			-- mih r0, 00000101
+			buffermem(2) := "0000011000000000";
 
+			-- mil r1, 00000001
+			buffermem(3) := "1010101010101010";
+
+			-- mih r1, 00000000
+			buffermem(4) := "1111111111111111";
+
+			-- add r1, r0
+			buffermem(5) := "0000000010110100";
+			init := false;
+		end if;
+
+		memdataready <= '0';
+
+		if  clk'event and clk = '1' then
+			ad := to_integer(unsigned(addressbus));
+
+			if readmem = '1' then -- Readiing :)
+				memdataready <= '1';
+				if ad >= blocksize then
+					databus <= (others => 'Z');
+				else
+					databus <= buffermem(ad);
+				end if;
+			elsif writemem = '1' then -- Writing :)
+				memdataready <= '1';
+				if ad < blocksize then
+					buffermem(ad) := databus;
+				end if;
+			elsif readmem = '0' then
+				databus <= (others => 'Z');
+			end if;
+		end if;
+	end process;
+end architecture behavioral;
